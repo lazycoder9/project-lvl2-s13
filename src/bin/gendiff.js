039c96869c5af748c4fd79c74eb5b9677bb4f188
ula9 <t.ulugbek.9@gmail.com>
2017-01-23 22:00:16 +0500

    fix gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 import program from 'commander';
+import fs from 'fs';
+
+let config1;
+let config2;
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('[options] <first_config> <second_config>')
+  .arguments('<first_config> <second_config>')
+  .action(function (firstConfig, secondConfig) {
+    config1 = JSON.parse(fs.readFileSync(firstConfig));
+    config2 = JSON.parse(fs.readFileSync(secondConfig));
+  })
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
+
+console.log(config1);
+console.log(config2);
