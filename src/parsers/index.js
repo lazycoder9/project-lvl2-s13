62de12fdf265c4c0ebf22587bd06749752f78303
ula9 <t.ulugbek.9@gmail.com>
2017-01-26 01:03:09 +0500

    fix another bullshit

--- src/parsers/index.js
+++ src/parsers/index.js
@@ -3,7 +3,7 @@ import yamlParser from './yamlParser';
 import iniParser from './iniParser';
 
 export default {
-  '.json': (obj1, obj2) => [jsonParser(obj1), jsonParser(obj2)],
-  '.yml': (obj1, obj2) => [yamlParser(obj1), yamlParser(obj2)],
-  '.ini': (obj1, obj2) => [iniParser(obj1), iniParser(obj2)],
+  '.json': (obj) => jsonParser(obj),
+  '.yml': (obj) => yamlParser(obj),
+  '.ini': (obj) => iniParser(obj),
 };
